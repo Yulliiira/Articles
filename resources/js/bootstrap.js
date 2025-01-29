87c89324705bfe7cb4d518d66aa374ede2b1cb7f
Yuliya Raskatova <yuliyaraskatova@MacBook-Air-Yuliya.local>
2025-01-29 16:02:41 +0400

    rollback app

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const btnAppValidMsg = document.querySelector(".app-run-valid-msg");
+
+if (btnAppValidMsg) {
+    btnAppValidMsg.addEventListener("click", function () {
+        window.axios.post("/messages/valid");
+    });
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user/{name}/{surname}', [PostController::class, 'index']);
-
+use Illuminate\Support\Facades\URL;
 
 
-Route::resource('events', EventController::class);
+Route::get('/current-url', function (){
+    return request()->fullUrl();
+});
+
+
+
